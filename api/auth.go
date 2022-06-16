fc688a40f4be694f752157808b4f3c302905206a
Below Bed <jordanmeta4677@gmail.com>
2022-06-17 02:40:26 +0800

    fix syntax

--- api/auth.go
+++ api/auth.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 )
 
-    func Handler(w http.ResponseWriter, r *http.Request) {
-	    fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-    }
+func Handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
